2c85c83cd5784f25e12c1b2d66ec7fe3804a5a35
Girish <adari.girishkumar@gmail.com>
2022-11-18 14:27:59 -0500

    inserting nodes in the singly linked lists

--- LinkedLists/SinglyLinkedList.py
+++ LinkedLists/SinglyLinkedList.py
@@ -13,11 +13,43 @@ class SLL:
         while node:
             yield node
             node = node.next
+    
+    def insertnode(self,value,position):
+        newNode = node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if position == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif position == -1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < position-1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
+                    self.tail = newNode
+
 
 
 a = SLL()
-a.head = node(1)
-a.tail = node(2)
-a.head.next = a.tail
-print(a.head.value,a.head.next.value,a.tail.value)
+# a.head = node(1)
+# a.tail = node(2)
+# a.head.next = a.tail
+# print(a.head.value,a.head.next.value,a.tail.value)
+a.head = node(5)
+a.insertnode(1,1)
+a.insertnode(2,1)
+a.insertnode(3,1)
+a.insertnode(4,1)
+a.insertnode(40,5)
 print([node.value for node in a])
